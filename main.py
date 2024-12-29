0cbddf4ff344740c252f0b536eabf9918c29c3ec
Bhuvanesh24 <bhuvaneshwaran813@gmail.com>
2024-12-29 22:20:35 +0530

    Added django

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="FastAPI Model Mediator")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8000","http://localhost:3000"],  
+    allow_origins=["andhra-django-production.up.railway.app","http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
